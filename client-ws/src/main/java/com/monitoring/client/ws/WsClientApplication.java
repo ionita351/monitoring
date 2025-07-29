e926d58d566e091ea66ca736b76acab21dccabe5
oleg <ionita@mail.ru>
2025-07-29 23:21:46 +0300

    client-ws

--- client-ws/src/main/java/com/monitoring/client/ws/WsClientApplication.java
+++ client-ws/src/main/java/com/monitoring/client/ws/WsClientApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.time.LocalDateTime;
@@ -16,6 +17,7 @@ import java.util.List;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan("com.monitoring")
 public class WsClientApplication implements CommandLineRunner {
 
--- client-ws/src/main/java/com/monitoring/client/ws/client/MeasurementWsClient.java
+++ client-ws/src/main/java/com/monitoring/client/ws/client/MeasurementWsClient.java
@@ -8,6 +8,10 @@ import com.monitoring.ws.wsdl.WsMeasurement;
 import com.monitoring.ws.wsdl.WsMeasurements;
 import com.monitoring.ws.wsdl.WsResponse;
 import jakarta.xml.bind.JAXBElement;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Component;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
@@ -17,8 +21,14 @@ import java.util.List;
 public class MeasurementWsClient  extends WebServiceGatewaySupport {
     private final ObjectFactory OBJECT_FACTORY = new ObjectFactory();
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     public ResponseDto sendOne(MeasurementDto measurement) {
         try {
+            ServiceInstance serviceInstance = discoveryClient.getInstances("server-ws").stream().findFirst().orElse(null);
+            getWebServiceTemplate().setDefaultUri(serviceInstance.getUri().toString() + "/ws");
+
             JAXBElement<WsResponse> wsResponse = (JAXBElement<WsResponse>) getWebServiceTemplate()
                     .marshalSendAndReceive(OBJECT_FACTORY.createReceiveOneRequest(toWsMeasurement(measurement)));
             return toResponseDto(wsResponse.getValue());
@@ -28,12 +38,20 @@ public class MeasurementWsClient  extends WebServiceGatewaySupport {
     }
 
     public ResponseDto sendMany(List<MeasurementDto> measurements) {
-        List<WsMeasurement> listWsMeasurements = measurements.stream().map(MeasurementWsClient::toWsMeasurement).toList();
-        WsMeasurements wsMeasurements = OBJECT_FACTORY.createWsMeasurements();
-        wsMeasurements.getMeasurements().addAll(listWsMeasurements);
-        JAXBElement<WsResponse> wsResponse = (JAXBElement<WsResponse>) getWebServiceTemplate()
-                .marshalSendAndReceive(OBJECT_FACTORY.createReceiveManyRequest(wsMeasurements));
-        return toResponseDto(wsResponse.getValue());
+        try {
+            ServiceInstance serviceInstance = discoveryClient.getInstances("server-ws").stream().findFirst().orElse(null);
+            getWebServiceTemplate().setDefaultUri(serviceInstance.getUri().toString() + "/ws");
+
+            List<WsMeasurement> listWsMeasurements = measurements.stream().map(MeasurementWsClient::toWsMeasurement).toList();
+            WsMeasurements wsMeasurements = OBJECT_FACTORY.createWsMeasurements();
+            wsMeasurements.getMeasurements().addAll(listWsMeasurements);
+
+            JAXBElement<WsResponse> wsResponse = (JAXBElement<WsResponse>) getWebServiceTemplate()
+                    .marshalSendAndReceive(OBJECT_FACTORY.createReceiveManyRequest(wsMeasurements));
+            return toResponseDto(wsResponse.getValue());
+        } catch (Throwable throwable) {
+            return ResponseDto.error(throwable.getMessage());
+        }
     }
 
     private static WsMeasurement toWsMeasurement(MeasurementDto dto) {
