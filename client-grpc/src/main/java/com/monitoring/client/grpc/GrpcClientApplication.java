17de4abb802afd112276e513d1f6a8679c526b81
oleg <ionita@mail.ru>
2025-07-30 22:19:34 +0300

    server & client grpc

--- client-grpc/src/main/java/com/monitoring/client/grpc/GrpcClientApplication.java
+++ client-grpc/src/main/java/com/monitoring/client/grpc/GrpcClientApplication.java
@@ -7,14 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-@SpringBootApplication
-@ComponentScan("com.monitoring")
+@SpringBootApplication(scanBasePackages = "com.monitoring")
+@EnableDiscoveryClient
 public class GrpcClientApplication implements CommandLineRunner {
 
     public static void main(String... args) {
